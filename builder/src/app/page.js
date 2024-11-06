1f2b41f6a7fa601edebc71a49084fb6ecdf9b5c5
Hashemi Rafsan <rafsanhashemi@gmail.com>
2024-11-06 06:06:43 +0600

    update config file

--- builder/src/app/page.js
+++ builder/src/app/page.js
@@ -2,24 +2,32 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import "sparnatural"
 import "sparnatural/dist/sparnatural.css"
 import config from "./config.json"
 
 export default function Home() {
     const sparnaturalRef = useRef(null);
+    const [queryString, setQueryString] = useState(null);
+
     useEffect(
         () => {
             sparnaturalRef?.current?.addEventListener("queryUpdated", (event) => {
-                console.log('Hello')
+                setQueryString(event.detail)
+                // console.log(event?.detail?.queryJson);
+                // console.log(event?.detail?.querySparqlJs);
+                // here : don't forget to call expandSparql so that core:sparqlString annotation is taken into account
+            });
+            sparnaturalRef?.current?.addEventListener("submit", (event) => {
+                console.log(queryString);
                 // console.log(event?.detail?.queryString);
                 // console.log(event?.detail?.queryJson);
                 // console.log(event?.detail?.querySparqlJs);
                 // here : don't forget to call expandSparql so that core:sparqlString annotation is taken into account
             });
         },
-        [],
+        [queryString],
     );
   return (
       <div className="App">
@@ -27,14 +35,14 @@ export default function Home() {
         <div id="ui-search">
           <spar-natural
               ref={sparnaturalRef}
-              src={'https://raw.githubusercontent.com/hashemirafsan/sparql-builder/refs/heads/main/config-v4.ttl'}
+              src={'https://raw.githubusercontent.com/hashemirafsan/sparql-builder/refs/heads/main/config-v5.ttl'}
               lang={'en'}
               defaultLang={'en'}
-              endpoint={'https://dbpedia.org/sparql'}
+              endpoint={'http://localhost:8080/sparql'}
               distinct={'true'}
               limit={'100'}
               debug={"false"}
-              submitButton={false}
+              submitButton={true}
           />
         </div>
       </div>
